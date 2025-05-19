b7d55c3ab8d7ab34921b4ad24597109120f37bc6
Finn <89312393+finnzxje@users.noreply.github.com>
2025-05-19 23:27:00 +0700

    refactor: when a user submit all quiz in a grammar - update lesson

--- backend/ToiYeuIT/src/main/java/com/example/toiyeuit/repository/lesson/GrammarQuizRepository.java
+++ backend/ToiYeuIT/src/main/java/com/example/toiyeuit/repository/lesson/GrammarQuizRepository.java
@@ -4,7 +4,9 @@ import com.example.toiyeuit.entity.lesson.GrammarQuiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GrammarQuizRepository extends JpaRepository<GrammarQuiz, Long> {
-
+    List<GrammarQuiz> findAllByGrammarId(Long grammarId);
 }
